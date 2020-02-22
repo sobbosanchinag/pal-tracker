656335a181a2b4619332a13dbc9d06cd3d84026a
Sobbosanchi Nag <sobbosanchi.nag@cognizant.com>
2020-02-22 14:51:42 +0530

    added EnvController and EnvControllerTest

---
+++ src/test/java/io/pivotal/pal/tracker/test/EnvControllerTest.java
@@ -0,0 +1,34 @@
+package io.pivotal.pal.tracker.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EnvControllerTest {
+
+    @LocalServerPort
+    int randomServerPort;
+
+    @Test
+    public void testImplementation() throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        String baseUrl = "http://localhost:"+randomServerPort+"/env";
+        Map<String, String> env = new HashMap<>();
+        env.put("PORT", "NOT SET");
+        env.put("MEMORY_LIMIT", "NOT SET");
+        env.put("CF_INSTANCE_INDEX", "NOT SET");
+        env.put("CF_INSTANCE_ADDR", "NOT SET");
+        ResponseEntity<Map> response = restTemplate.getForEntity(new URL(baseUrl).toString(), Map.class);
+        assertEquals(env,response.getBody());
+    }
+}
